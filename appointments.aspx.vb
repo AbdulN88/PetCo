f553cda8b2c367c7d4c653b34ab08281227560a3
CypherDragoon88 <abdul_naqshband@live.co.uk>
2016-01-20 00:58:07 +0000

    Create appointments.aspx.vb

---
+++ appointments.aspx.vb
@@ -0,0 +1,9 @@
+@@ -0,0 +1,8 @@
+﻿
+Partial Class appointmentpgaspx
+    Inherits System.Web.UI.Page
+
+    Protected Sub TextBoxAddresslineone_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxAddresslineone.TextChanged
+
+    End Sub
+End Class
