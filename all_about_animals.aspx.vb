1cb4da0ebb49163f9c8ca53a4018822014feeb1a
CypherDragoon88 <abdul_naqshband@live.co.uk>
2016-01-20 00:56:56 +0000

    Create all_about_animals.aspx.vb

---
+++ all_about_animals.aspx.vb
@@ -0,0 +1,9 @@
+@@ -0,0 +1,8 @@
+﻿
+Partial Class all_about_animals
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+End Class
